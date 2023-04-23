ad372571640348932ba7ab1c030b2f473a0babdb
Isaac Cohen <issc29@gmail.com>
2023-04-23 15:11:36 -0400

    remove feature classes text

--- src/components/featured-classes.js
+++ src/components/featured-classes.js
@@ -28,7 +28,6 @@ export default function FeaturedClasses() {
           <div className="text-dark-blue my-10">
             <hr className="border-2 border-dark-blue mx-auto w-1/2 mb-4" />
             <div className="text-center text-5xl">FEATURED CLASSES:</div>
-            <div className="text-center text-5xl">New ones every week!</div>
             <div className="flex flex-col sm:flex-row justify-center items-center space-y-3 sm:space-y-0 sm:space-x-6 mt-5">
             {data.site.featuredClasses.map((featuredClass) => (
                 <button className="bg-dark-blue text-lg text-white hover:bg-white hover:text-dark-blue font-bold py-5 sm:py-2 px-4 w-full sm:w-60 h-auto sm:h-24 rounded-md">{featuredClass.title}</button>
